af172fea0f1c9879a0fa7d9547f6ed11997fa97b
Nikita Mezhnyakov <mezhng@gmail.com>
2023-10-21 12:03:22 +0300

    Update setup of package.

--- setup.py
+++ setup.py
@@ -2,8 +2,12 @@ from distutils.core import setup
 
 setup(
     name='moex_connector',
-    version='0.3.1',
+    version='0.3.2',
     description='MOEX API connector',
     author='meznick',
-    packages=['moex']
+    packages=['moex'],
+    install_requires=[
+        'requests>=2.28,<3',
+        'pandas>2,<3',
+    ]
 )
